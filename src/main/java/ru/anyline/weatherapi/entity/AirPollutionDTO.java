1d83ba9028c85bf331749476d8acd2d1edb83003
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2024-10-09 10:33:32 +0300

    air pollution api

---
+++ src/main/java/ru/anyline/weatherapi/entity/AirPollutionDTO.java
@@ -0,0 +1,37 @@
+package ru.anyline.weatherapi.entity;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class AirPollutionDTO {
+
+    private List<Double> coord;
+    private List<PollutionData> list;
+
+    @Data
+    public static class PollutionData {
+        private long dt;
+        private Main main;
+        private Components components;
+
+        @Data
+        public static class Main {
+            private int aqi;
+
+        }
+        @Data
+        public static class Components {
+            private double co;
+            private double no;
+            private double no2;
+            private double o3;
+            private double so2;
+            private double pm2_5;
+            private double pm10;
+            private double nh3;
+
+        }
+    }
+}
