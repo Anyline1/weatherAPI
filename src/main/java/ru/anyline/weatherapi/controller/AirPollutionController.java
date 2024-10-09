b0653f6724618b3602664ce8b13284c28f99583a
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2024-10-09 11:33:53 +0300

    air pollution api

--- src/main/java/ru/anyline/weatherapi/controller/AirPollutionController.java
+++ src/main/java/ru/anyline/weatherapi/controller/AirPollutionController.java
@@ -1,9 +1,11 @@
 package ru.anyline.weatherapi.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.anyline.weatherapi.entity.AirPollutionDTO;
+import ru.anyline.weatherapi.entity.AirPollutionData;
 import ru.anyline.weatherapi.service.AirPollutionService;
 
 @RestController
@@ -12,9 +14,9 @@ public class AirPollutionController {
 
     private AirPollutionService airPollutionService;
 
-    @GetMapping("/fetchair")
-    public String fetchAirPollutionData() {
-        airPollutionService.fetchAirPollutionData();
-        return "to do";
+    @GetMapping("/airpollution")
+    public ResponseEntity<AirPollutionDTO> fetchAirPollutionData() {
+        AirPollutionDTO data = airPollutionService.fetchAirPollutionData();
+        return ResponseEntity.ok(data);
     }
 }
