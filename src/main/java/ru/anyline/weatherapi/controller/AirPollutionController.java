0151477653f619ebe54f466a73d5f614d720f922
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2024-10-10 14:27:16 +0300

    fix

--- src/main/java/ru/anyline/weatherapi/controller/AirPollutionController.java
+++ src/main/java/ru/anyline/weatherapi/controller/AirPollutionController.java
@@ -1,8 +1,10 @@
 package ru.anyline.weatherapi.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.anyline.weatherapi.entity.AirPollutionDTO;
 import ru.anyline.weatherapi.service.AirPollutionService;
@@ -11,10 +13,14 @@ import ru.anyline.weatherapi.service.AirPollutionService;
 @AllArgsConstructor
 public class AirPollutionController {
 
-    private AirPollutionService airPollutionService;
+    private final AirPollutionService airPollutionService;
 
     @GetMapping("/airpollution")
-    public ResponseEntity<AirPollutionDTO> fetchAirPollutionData() {
+    public ResponseEntity<AirPollutionDTO> fetchAirPollutionData(@RequestParam String city) {
+
+        if (!city.matches("[a-zA-Z ]+") || city.isEmpty() || city.length() > 49) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
         AirPollutionDTO data = airPollutionService.fetchAirPollutionData();
         return ResponseEntity.ok(data);
     }
